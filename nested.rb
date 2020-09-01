79b80cd52ee48b098d3cf46a32c1ab6813e98b07
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-09-01 02:30:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -103,7 +103,6 @@ def changing_alan
       }
     }
   programmer_hash[:alan_kay][:known_for] = "GUI"
-  
   programmer_hash
 end
 
@@ -126,5 +125,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:dennis_ritchie][:languages] = "Assembly"
 
 end
